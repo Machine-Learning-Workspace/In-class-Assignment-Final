48f91fa86014c400bcfec6319b33faa80fe3bc8b
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-07-02 13:00:04 +0530

    model-added

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+def load_model():
+    import torch
+    from torch import nn
+    from model import ScorePredictionModel
+    model_state = torch.load("model/In-class-Assignment-Final-Model.ph")
+    model = ScorePredictionModel()
+    model.load_state_dict(model_state)
+    return model
+
+def predict(x):
+    import torch
+    import numpy as np
+    model = load_model()
+    x = torch.from_numpy(np.array([0.12])).float()
+    val = model(x).detach().numpy()
+    return float(val)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def root():
+    val = predict(0.21)
+    print(val)
+    return 'In-class-Assignment-Final-Model'
 
 
 if __name__ == '__main__':
